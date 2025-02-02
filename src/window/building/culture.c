18869df3792026430fa77bf74b2f3d771454385a
eugenikus8 <51211736+eugenikus8@users.noreply.github.com>
2025-02-02 03:03:29 +0200

    Update src/window/building/culture.c
    Co-authored-by: José Cadete <crudelios@gmail.com>

--- src/window/building/culture.c
+++ src/window/building/culture.c
@@ -1215,8 +1215,7 @@ void window_building_draw_lighthouse(building_info_context *c)
 
         if (!c->has_road_access) {
             window_building_draw_description_at(c, 80, 69, 25);
-        }
-        else if (building_monument_has_labour_problems(b)) {
+        } else if (building_monument_has_labour_problems(b)) {
             text_draw_multiline(translation_for(TR_BUILDING_LIGHTHOUSE_NEEDS_WORKERS),
                 c->x_offset + 32, c->y_offset + 80, 15 * c->width_blocks, 0, FONT_NORMAL_BLACK, 0);
         } else {
