f7c09d8c33e1c4742e785ffc9cd7708fd3399ed8
Areldir <72540647+Areldir@users.noreply.github.com>
2026-06-07 13:14:40 +1000

    Update english.c

--- src/translation/english.c
+++ src/translation/english.c
@@ -2109,11 +2109,11 @@ static translation_string all_strings[] = {
     {TR_CONFIG_SHOW_ELEVATION_DESIRABILITY, "Show elevation desirability in overlay"},
     {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_1, "(Supports)"},
     {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_2, "(Facade)"},
-    {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_1_TEXT, "Masons are laying the stonework and erecting towering pillars for a colossal triumphal arch, forged to immortalize the valor of your armies and proclaim their victories to generations yet unborn."},
-    {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_2_TEXT, "Skilled artisans are completing the facade of the grand arch with finely carved reliefs, polished stonework, and statues honoring the courage of your soldiers, giving the monument a dignity worthy of the city’s triumphs."},
-    {TR_BUILDING_TRIUMPHAL_ARCH_CONSTRUCTION_DESC, "The triumphal arch does not need resources from your city to be build. All neccessary resources and laborers will be supplied by rome to extol your military success."},
+    {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_1_TEXT, "Masons are laying the stonework and erecting towering pillars for a colossal arch that will immortalize the valor of your armies and proclaim their triumphant deeds to generations across the ages."},
+    {TR_BUILDING_TRIUMPHAL_ARCH_PHASE_2_TEXT, "Skilled artisans are completing the facade of the grand arch with finely carved reliefs, polished stonework and statues honoring the courage of your soldiers, giving the monument a dignity worthy of a Roman triumph."},
+    {TR_BUILDING_TRIUMPHAL_ARCH_CONSTRUCTION_DESC, "The triumphal arch does not need resources from your city to be build. All necessary resources and laborers will be supplied by Rome."},
     {TR_CITY_MESSAGE_TITLE_TRIUMPHAL_ARCH_COMPLETE, "Triumphal arch completed"},
-    {TR_CITY_MESSAGE_TEXT_TRIUMPHAL_ARCH_COMPLETE, "The triumphal arch now stands complete, its towering stonework and finely carved facade honoring the courage of our soldiers and the victories they have secured for the city. May it stand for generations as a symbol of strength, sacrifice, and civic pride."},
+    {TR_CITY_MESSAGE_TEXT_TRIUMPHAL_ARCH_COMPLETE, "The triumphal arch now stands complete, its towering stonework and finely carved facade honoring the courage of our soldiers and the victories they have secured for the city. May it stand for generations as a symbol of strength, sacrifice and civic pride."},
     {TR_BUILDING_TRIUMPHAL_ARCH_SUPPLIED_BY_ROME, "(Supplied by Rome)"}
 };
 
