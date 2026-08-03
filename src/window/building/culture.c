9240ab06319f0d66f21898564c3e51b7ac5e4ebf
eugenikus8 <eugenikus8@gmail.com>
2026-08-03 12:33:49 +0300

    fix sound

--- src/window/building/culture.c
+++ src/window/building/culture.c
@@ -1176,7 +1176,7 @@ void window_building_draw_arena(building_info_context *c)
     c->help_id = 73;
     building *b = building_get(c->building_id);
 
-    window_building_play_sound(c, "wavs/arena.wav");
+    window_building_play_sound(c, "wavs/colloseum.wav");
     outer_panel_draw(c->x_offset, c->y_offset, c->width_blocks, c->height_blocks);
     inner_panel_draw(c->x_offset + 16, c->y_offset + 168, c->width_blocks - 2, 7);
     window_building_draw_employment(c, 172);
